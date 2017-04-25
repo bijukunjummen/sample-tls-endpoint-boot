0bada70f3f9408f96e049bbff0db43085e80f837
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-04-25 10:43:04 -0700

    SSL sample

---
+++ src/main/java/org/bk/DemoController.java
@@ -0,0 +1,16 @@
+package org.bk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
+
+
---
+++ src/main/java/org/bk/DemoSecuredApplication.java
@@ -0,0 +1,12 @@
+package org.bk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecuredApplication.class, args);
+	}
+}
